da3c07e53ccb8092fe56e9afc366be72fe04f83d
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-02-27 00:58:36 +0600

    books and users components and routes added

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router"
 import BodyComponent from './../components/BodyComponent.vue'
 import LoginComponent from '../components/LoginComponent.vue'
+import BooksComponent from '../components/books/BooksComponent.vue'
+import BookComponent from '../components/books/BookComponent.vue'
+import BooksAdmin from '../components/books/BooksAdmin.vue'
+import BookEdit from '../components/books/BookEdit.vue'
+import UsersComponent from '../components/books/UsersComponent.vue'
+import UserEdit from '../components/books/UserEdit.vue'
 
 const routes = [
     {
         path: '/',
-        name: 'Home',
+        name: 'home',
         component: BodyComponent,
     },
 
@@ -13,6 +19,42 @@ const routes = [
         path: '/login',
         name: 'login',
         component: LoginComponent,
+    },
+
+    {
+        path: '/books',
+        name: 'books',
+        component: BooksComponent,
+    },
+
+    {
+        path: '/books/:bookName',
+        name: 'book',
+        component: BookComponent,
+    },
+
+    {
+        path: '/admin/books',
+        name: 'booksAdmin',
+        component: BooksAdmin,
+    },
+
+    {
+        path: '/admin/books/:bookId',
+        name: 'bookEdit',
+        component: BookEdit,
+    },
+
+    {
+        path: '/admin/users',
+        name: 'users',
+        component: UsersComponent,
+    },
+
+    {
+        path: '/admin/users/:userId',
+        name: 'editUser',
+        component: UserEdit,
     },
 ]
 
