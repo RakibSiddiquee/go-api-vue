e1d26e125b35a54c4f215d82627e072fab13ec2d
RakibSiddiquee <rakibsiddiquee@gmail.com>
2024-03-13 01:41:03 +0600

    Book component updated

--- src/components/books/BookComponent.vue
+++ src/components/books/BookComponent.vue
@@ -1,8 +1,18 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col">
-                <h1 class="mt-3">Book</h1>
+            <div class="col-md-2">
+                <img class="img-fluid img-thumbnail" :src="`${imgPath}/covers/${book.slug}.jpg`" alt="cover" />
+            </div>
+            <div class="col-md-10">
+                <h3 class="mt-3">{{ book.title }}</h3>
+                <p>
+                    <strong>Author:</strong> {{ book.author?.author_name }}<br>
+                    <strong>Published:</strong> {{ book.publication_year }}
+                </p>
+                <p>
+                    {{ book.description }}
+                </p>
             </div>
         </div>
     </div>
@@ -17,7 +27,7 @@
             }
         },
         created() {
-            fetch(process.env.VUE_APP_API_URL + "/books" + this.$route.params.slug)
+            fetch(process.env.VUE_APP_API_URL + "/books/" + this.$route.params.slug)
             .then(response => response.json())
             .then(data => {
                 if (data.error) {
